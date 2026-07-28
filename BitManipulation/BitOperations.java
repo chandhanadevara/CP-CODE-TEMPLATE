4767dfdcf98080887e9465e1f716cf448ecfad82
Chandhana <chandhanadevara1729@gmail.com>
2026-07-28 21:35:36 +0530

    Add BitOperations class for bit manipulation
    Implemented methods for bit manipulation including setting, clearing, toggling, and getting the kth bit.

---
+++ BitManipulation/BitOperations.java
@@ -0,0 +1,22 @@
+public class BitOperations {
+
+    // Set the kth bit to 1
+    static int setKthBit(int n, int k) {
+        return n | (1 << k);
+    }
+
+    // Clear the kth bit to 0
+    static int clearKthBit(int n, int k) {
+        return n & ~(1 << k);
+    }
+
+    // Toggle the kth bit
+    static int toggleKthBit(int n, int k) {
+        return n ^ (1 << k);
+    }
+
+    // Get the kth bit
+    static int getKthBit(int n, int k) {
+        return (n >> k) & 1;
+    }
+}
